5642cf58d5b219911cc192a2ddf6edee57d69b70
salah359 <l39979881@student.ua92.ac.uk>
2025-12-29 21:23:12 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -190,7 +190,8 @@ function sendToWhatsApp() {
     message += "Please let me know the delivery details! ✨";
     
     // Note: Change 'YOUR_PHONE_NUMBER' to your actual WhatsApp number (e.g., 970599000000)
-    const phoneNumber = "074444450936"; 
+    const phoneNumber = "07444450936"; 
     window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`, '_blank');
 }
+
 
